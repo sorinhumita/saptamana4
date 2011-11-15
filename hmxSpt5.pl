5db9f594e38046b9247bc879125b2e24ff4b8b78
Sorin Humita <xx@yy.com>
2011-11-15 17:10:15 +0200

    saptamana a 8-a

--- hmxSpt5.pl
+++ hmxSpt5.pl
@@ -1,36 +1,16 @@
 
 use strict;
 
-open (FILE, "situatie1.txt") or die $!; #deschidere in mod citire.
-my $c=0;
-my $d=0;
-my $s2=0;
-my $nume=0;
+open (FILE, "situatie.txt") or die $!; #deschidere in mod citire.
 while (my $linie = <FILE>) 
 	{
-		my @list = split(" ",$linie);
-		$nume  = substr @list[0], 4;
-		print uc $nume." ";
-		if (scalar(@list) > 1 )
-		{
-		my @prenume_list = split("-",@list[1]);
-		if (scalar(@prenume_list) < 1 )
-		{
-		substr(@list[1], 0,1,uc substr(@list[1],0,1));
-		print @list[1]; 
-		}	  
-		else
-		{
-		 substr(@prenume_list[0], 0,1,uc substr(@prenume_list[0],0,1));
-		 substr(@prenume_list[1], 0,1,uc substr(@prenume_list[1],0,1));
-		 print @prenume_list[0]." ".@prenume_list[1];	
-		}
-		}
-		my @list_nota = split("- ",$linie);
-		if (substr(@list_nota[1],1,1) eq "<")
-			{print " ".substr(@list_nota[1],0,1)."\n";} 
-		else
-			{print " ".substr(@list_nota[1],0,2)."\n";} 
-	
+	chomp($linie);
+	if ( $linie =~ m/^<li>(\w+)\s+([a-z ]+).+(\d+)$/ ) {
+	print "$linie\t\t--> $1 +++ $2 +++ $3 \n";
+#	$1 - prima paranteza 
+#	$2 - a doua paranteza 
+#	tot asa
 	}
-close(FILE);
+	}
+	
+	
